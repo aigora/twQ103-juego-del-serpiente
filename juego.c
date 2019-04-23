39b88870460101af26a7ad2e24c8d75d42fdce13
ElenaMarhuenda <38626737+ElenaMarhuenda@users.noreply.github.com>
2019-04-23 23:20:13 +0200

    juego.c
    Después de que Mao añadiese la declaración de las funciones he añadido yo la iniciación de los elementos, las instrucciones y la creación del campo de juego.

--- juego.c
+++ juego.c
@@ -38,3 +38,59 @@ int main()
 	return 0;
 }
 
+
+//Iniciar todos los elementos 
+void inicio (int *tam, char campo[V][H])
+{
+	int i;
+	//Cabeza de la serpiente
+	snake[0].x = 32;
+	snake[0].y = 10;
+	//Tamano
+	*tam = 4;
+	//Coordenadas de la fruta
+	srand(time(NULL));// para generar la fruita aleatoria con el tiempo
+	fruta.x = rand() % (H-1);// rand () para generar aleatoria
+	fruta.y = rand() % (V-1);
+	
+	while(fruta.x == 0)
+	{
+		fruta.x = rand() % (H-1);
+	}
+	while(fruta.y == 0)
+	{
+		fruta.y = rand() % (V-1);
+	}
+	for(i = 0; i < *tam; i++)
+	{
+		snake[i].ModX = 1;
+		snake[i].ModY = 0;
+	}
+	
+	Intro_Campo(campo);
+	Intro_Datos(campo, *tam);
+}
+
+//Creacion del campo de juego
+void Intro_Campo(char campo[V][H])
+{
+	int i, j;
+	for (i = 0; i < V; i++)
+	{
+		for (j = 0; j < H; j++)
+		{
+			if(i == 0 || i == V-1)
+			{
+				campo[i][j] = '=';
+			}
+			else if (j == 0 || j == H-1)
+			{
+				campo[i][j] = '||';
+			}
+			else
+			{
+				campo[i][j] = ' ';
+			}
+		}
+	}
+}
